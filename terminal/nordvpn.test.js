5ea89020a1059130a58bdde67eeda59d44c786ed
Kenny Hung <kennysubs+github@outlook.com>
2020-03-31 12:01:22 +0100

    ES Lint

--- terminal/nordvpn.test.js
+++ terminal/nordvpn.test.js
@@ -6,130 +6,131 @@ const allCities = require('all-the-cities');
 const { command, username, password } = dummyNordVpn;
 
 describe('NordVPN', () => {
-  let terminal;
-  let nordvpn;
+    let terminal;
+    let nordvpn;
 
-  beforeEach(async () => {
-    await dummyNordVpn.clear();
-    terminal = new Terminal();
-    const config = { executable: command };
-    nordvpn = new NordVpn(config);
-  });
-
-  describe('connection status', () => {
-    it('disconnected', async () => {
-      const status = await nordvpn.getConnectionStatus();
-
-      expect(status).to.equal(NordVpn.status.connection.DISCONNECTED);
-    });
-  })
-
-  describe('user status', () => {
-    it('logged in', async () => {
-      await terminal.execute(`${command} login -u ${username} -p ${password}`);
-
-      const status = await nordvpn.getUserStatus();
-
-      expect(status).to.equal(NordVpn.status.user.LOGGEDIN);
+    beforeEach(async () => {
+        await dummyNordVpn.clear();
+        terminal = new Terminal();
+        const config = { executable: command };
+        nordvpn = new NordVpn(config);
     });
 
-    it('logged out', async () => {
-      const status = await nordvpn.getUserStatus();
+    describe('connection status', () => {
+        it('disconnected', async () => {
+            const status = await nordvpn.getConnectionStatus();
 
-      expect(status).to.equal(NordVpn.status.user.LOGGEDOUT);
-    });
-  });
-
-  describe('login', () => {
-    it('can login', async () => {
-      expect(await nordvpn.getUserStatus()).to.equal(NordVpn.status.user.LOGGEDOUT);
-
-      const result = await nordvpn.login({ username, password });
-
-      expect(result).to.be.true;
-      expect(await nordvpn.getUserStatus()).to.equal(NordVpn.status.user.LOGGEDIN);
+            expect(status).to.equal(NordVpn.status.connection.DISCONNECTED);
+        });
     });
 
-    it('cannot login with incorrect credentials', async () => {
-      expect(await nordvpn.getUserStatus()).to.equal(NordVpn.status.user.LOGGEDOUT);
+    describe('user status', () => {
+        it('logged in', async () => {
+            await terminal.execute(`${command} login -u ${username} -p ${password}`);
 
-      const result = await nordvpn.login({ username: 'not_username', password: password + password });
+            const status = await nordvpn.getUserStatus();
 
-      expect(result).to.be.false;
-      expect(await nordvpn.getUserStatus()).to.equal(NordVpn.status.user.LOGGEDOUT);
-    })
-  });
+            expect(status).to.equal(NordVpn.status.user.LOGGEDIN);
+        });
 
-  describe.only('connect', () => {
-    it('can connect without city', async () => {
-      await nordvpn.login({ username, password });
+        it('logged out', async () => {
+            const status = await nordvpn.getUserStatus();
 
-      const result = await nordvpn.connect();
-
-      const status = await nordvpn.getConnectionStatus();
-      expect(result).to.be.true;
-      expect(status).to.equal(NordVpn.status.connection.CONNECTED);
+            expect(status).to.equal(NordVpn.status.user.LOGGEDOUT);
+        });
     });
 
-    it('can connect with city', async () => {
-      await nordvpn.login({ username, password });
+    describe('login', () => {
+        it('can login', async () => {
+            expect(await nordvpn.getUserStatus()).to.equal(NordVpn.status.user.LOGGEDOUT);
 
-      const result = await nordvpn.connect('New_York_City');
+            const result = await nordvpn.login({ username, password });
 
-      const status = await nordvpn.getConnectionStatus();
-      expect(result).to.be.true;
-      expect(status).to.equal(NordVpn.status.connection.CONNECTED);
+            expect(result).to.be.true;
+            expect(await nordvpn.getUserStatus()).to.equal(NordVpn.status.user.LOGGEDIN);
+        });
+
+        it('cannot login with incorrect credentials', async () => {
+            expect(await nordvpn.getUserStatus()).to.equal(NordVpn.status.user.LOGGEDOUT);
+
+            const result = await nordvpn.login({ username: 'not_username', password: password + password });
+
+            expect(result).to.be.false;
+            expect(await nordvpn.getUserStatus()).to.equal(NordVpn.status.user.LOGGEDOUT);
+        });
     });
 
-    it('can connect with country code', async () => {
-      await nordvpn.login({ username, password });
+    describe('connect', () => {
+        it('can connect without city', async () => {
+            await nordvpn.login({ username, password });
 
-      const result = await nordvpn.connect('US');
+            const result = await nordvpn.connect();
 
-      const status = await nordvpn.getConnectionStatus();
-      expect(result).to.be.true;
-      expect(status).to.equal(NordVpn.status.connection.CONNECTED);
-    })
+            const status = await nordvpn.getConnectionStatus();
+            expect(result).to.be.true;
+            expect(status).to.equal(NordVpn.status.connection.CONNECTED);
+        });
 
-    it('cannot connect with incorrect city', async () => {
-      await nordvpn.login({ username, password });
+        it('can connect with city', async () => {
+            await nordvpn.login({ username, password });
 
-      const result = await nordvpn.connect('Not_A_City');
+            const result = await nordvpn.connect('New_York_City');
 
-      const status = await nordvpn.getConnectionStatus();
-      expect(result).to.be.false;
-      expect(status).to.equal(NordVpn.status.connection.DISCONNECTED);
-    })
-  })
+            const status = await nordvpn.getConnectionStatus();
+            expect(result).to.be.true;
+            expect(status).to.equal(NordVpn.status.connection.CONNECTED);
+        });
 
-  describe('geography', async () => {
-    it('can get all countries', async () => {
-      const countries = await nordvpn.getCountries();
+        it('can connect with country code', async () => {
+            await nordvpn.login({ username, password });
 
-      expect(countries).to.include('GB');
+            const result = await nordvpn.connect('US');
+
+            const status = await nordvpn.getConnectionStatus();
+            expect(result).to.be.true;
+            expect(status).to.equal(NordVpn.status.connection.CONNECTED);
+        });
+
+        it('cannot connect with incorrect city', async () => {
+            await nordvpn.login({ username, password });
+
+            const result = await nordvpn.connect('Not_A_City');
+
+            const status = await nordvpn.getConnectionStatus();
+            expect(result).to.be.false;
+            expect(status).to.equal(NordVpn.status.connection.DISCONNECTED);
+        });
     });
 
-    it('can get all cities', async () => {
-      const countries = (await nordvpn.getCountries())
-        .slice(0, 5); // Doing all of them takes a short time
-      const returnedCities = await Promise.all(countries
-        .map(async country => {
-          return {
-            cities: await nordvpn.getCities(country),
-            country
-          }})
-      );
-        
-      returnedCities.forEach(({country, cities}) => {
-        const citiesInCountry = allCities
-          .filter(it => it.country === country)
-          .map(it => it.name);
-        expect(cities).not.to.be.empty;
-        cities.forEach(city => {
-          expect(city[0].toUpperCase()).to.equal(city[0]);
-          expect(citiesInCountry).to.include(city.split('_').join(' '));
-        })
-      })
-    })
-  })
+    describe('geography', async () => {
+        it('can get all countries', async () => {
+            const countries = await nordvpn.getCountries();
+
+            expect(countries).to.include('GB');
+        });
+
+        it('can get all cities', async () => {
+            const countries = (await nordvpn.getCountries())
+                  .slice(0, 5); // Doing all of them takes a short time
+            const returnedCities = await Promise.all(countries
+                                                     .map(async country => {
+                                                         return {
+                                                             cities: await nordvpn.getCities(country),
+                                                             country
+                                                         };
+                                                     })
+                                                    );
+
+            returnedCities.forEach(({country, cities}) => {
+                const citiesInCountry = allCities
+                      .filter(it => it.country === country)
+                      .map(it => it.name);
+                expect(cities).not.to.be.empty;
+                cities.forEach(city => {
+                    expect(city[0].toUpperCase()).to.equal(city[0]);
+                    expect(citiesInCountry).to.include(city.split('_').join(' '));
+                });
+            });
+        });
+    });
 });
