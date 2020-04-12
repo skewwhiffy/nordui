4b37069212eeb33d900eccb8abbcc47752a7c598
Kenny Hung <kennysubs+github@outlook.com>
2020-04-12 15:40:57 +0100

    Remove status poller from status.vue

--- src/components/Status.vue
+++ src/components/Status.vue
@@ -6,12 +6,6 @@
 </template>
 
 <script>
-import { ipcRenderer } from 'electron';
-import Waiter from '../util/waiter';
-import StatusPoller from '../foreground/status.poller';
-
-let poller = {};
-
 export default {
   name: 'Status',
   props: {
@@ -29,17 +23,6 @@ export default {
         CONNECTED: 'Connected'
       }[this.status] || 'Not understood';
     }
-  },
-  created: function() {
-    const waiter = new Waiter();
-    poller = new StatusPoller({ ipcRenderer, ms: 1000, waiter });
-    poller.onChange(() => {
-      this.status = poller.status;
-    });
-  },
-  beforeDestroy() {
-    poller.destroy();
-    poller = {};
   }
 };
 </script>
